d1ccfa65c7bf0cb588fff349be6d7b8795d8058b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-12-01 17:37:18 +0100

    Factoring out a new project just containing the stuff related to mapping via annotation.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
-module org.meeuw.annotations {
+module org.meeuw.mapping.annotations {
     requires static lombok;
-
     requires com.fasterxml.jackson.databind;
     requires org.slf4j;
 
-    exports org.meeuw.mapping.json;
-    exports org.meeuw.mapping.impl;
     exports org.meeuw.mapping.annotations;
+    exports org.meeuw.mapping;
 
 }
